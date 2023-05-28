dbf307d9aed2f88d1c41afeaa3c9d874d6f4b6d8
Eric Reyes <ericreyes1109@gmail.com>
2023-05-27 21:52:29 -0500

    updated my contact page and now works

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,33 @@
 import React from "react";
-
+import emailjs from 'emailjs-com';
 
 function Contact() {
-    return
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_2pvt768', 'template_nq1bxev', e.target, 'xLwL3S1W24bVkg-I5')
+        .then((result) => {
+            window.location.reload()
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+
+
+    return (
+        <form onSubmit={sendEmail}>
+            <label className="contact">Name</label>
+        <input className="input-info" type='text' name="from_name" style={{alignItems: 'center'}} />
+        <label className="contact">Email</label>
+        <input className="input-info" type='email' name="from_email" style={{alignItems: 'center'}} />
+        <label className="contact">Message</label>
+        <textarea  className="description" type='text' name="message" style={{alignItems: 'center'}} />
+
+        <input type="submit" value="Send" className="contact-submit" />
+        </form>
+    )
 }
 
 export default Contact;
